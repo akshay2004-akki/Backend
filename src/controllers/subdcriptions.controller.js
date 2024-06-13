1d679747fabd0124f8c857c7530175f984c68ffe
Akshay anand <akshay1622004@gmail.com>
2024-06-13 06:49:03 +0530

    add toggle subscription

--- src/controllers/subdcriptions.controller.js
+++ src/controllers/subdcriptions.controller.js
@@ -8,7 +8,40 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    const userId = req.user?._id;
     // TODO: toggle subscription
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(404,"Invalid channel ID");
+    }
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(404, "User does not exist");
+    }
+
+    const channel = await User.findById(channelId);
+    if(!channel){
+        throw new ApiError(400,"Channel does not exist");
+    }
+
+
+    const user = await User.findById(userId);
+    if(!user){
+        throw new ApiError(400, "User doest not exist");
+    }
+
+    try {
+        const subscription = await Subscription.findOne({subscriber : userId, channel : channelId});
+        if(subscription){
+            await Subscription.deleteOne({_id : subscription._id});
+            return res.status(200).json(new ApiResponse(200,null,"Unsubscribed successfully"))
+        }
+        else{
+            await Subscription.create({subscriber : userId, channel : channelId});
+            return res.status(200).json(new ApiResponse(200,null, "Subscribed successfully"))
+        }
+    } catch (error) {
+        throw new ApiError(409,error?.message)
+    }
 })
 
 // controller to return subscriber list of a channel
