ebe7ec54f9ab5269dc14f725a1a09fe633e27c5e
Akshay anand <akshay1622004@gmail.com>
2024-06-17 13:44:19 +0530

    add health check and completed this project

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ import playlistRouter from "./routes/playlist.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
  
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
@@ -32,5 +34,6 @@ app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 export default app;
